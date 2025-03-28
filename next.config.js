da4ffb8b8e7dea7ba1f7ee74ea9fb7db00114053
CryptoMisfitSociety <cryptomisfitsociety@gmail.com>
2025-03-28 09:03:10 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,17 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "api.dicebear.com"],
   },
+  // Add this to explicitly set the port
+  serverOptions: {
+    port: 3003, // Or any other available port
+  }
 };
 
 if (process.env.NEXT_PUBLIC_TEMPO) {
   nextConfig["experimental"] = {
-    // NextJS 13.4.8 up to 14.1.3:
-    // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
-    // NextJS 14.1.3 to 14.2.11:
     swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]],
-
-    // NextJS 15+ (Not yet supported, coming soon)
   };
 }
 
